fb3ff46b3d14eadb53160a959b44838330723d77
Dhananjay <d.gupta@3mdigital.co.in>
2017-05-04 13:35:42 +0530

    Initial commit after adding required files

---
+++ app/src/main/java/com/learn/androidapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.learn.androidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.learn.androidapplication.android.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
