ecb846e1a363a555014b0c7f9fde04960fe30e47
Dhananjay <d.gupta@3mdigital.co.in>
2017-05-04 14:51:29 +0530

    changes

--- app/src/main/java/com/learn/androidapplication/MainActivity.java
+++ app/src/main/java/com/learn/androidapplication/MainActivity.java
@@ -1,16 +1,20 @@
 package com.learn.androidapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.learn.androidapplication.android.R;
 
+public class MainActivity extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity {
 
+
+    int one;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
